81c71d2111dfd7a597a1422b23a5b1c2247e1c78
Lucas <lucascan.geo@gmail.com>
2025-04-06 01:13:46 -0300

    adding landing page locators

--- pageObjects/landing-page.ts
+++ pageObjects/landing-page.ts
@@ -1,16 +1,27 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
 
+
+
 export class LandingPage {
     private readonly page: Page;
 
     constructor(page: Page) {
         this.page = page;
-        
+
         this.navBelt = this.page.locator('#nav-belt');
         this.navMain = this.page.locator('#nav-main');
+
+        this.navLogo = this.page.getByLabel(/Amazon.com.br/);
+        this.navLocationButton = this.page.locator('#nav-global-location-popover-link');
+        this.navSearchField = 
+
         this.pageContent = this.page.locator('#pageContent');
         this.rhfContainer = this.page.locator('#rhf-container');
 
+
+
+
     }
 }
+
